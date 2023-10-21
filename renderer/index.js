4936e2e1abd3c335bcb773ec73357c2ff62deb3d
JimmyBinoculars <91781252+JimmyBinoculars@users.noreply.github.com>
2023-10-21 08:26:05 +0100

    Changed fn to f

--- renderer/index.js
+++ renderer/index.js
@@ -1,7 +1,7 @@
 const lib = require("text-renderer");
 
 let cube = new lib.item("cube", [0,0,0]/*pos*/, [0,0,0]/*rot*/, [2, 2, 2]/*scale*/);
-let fn = 0;
+let f = 0;
 let camera = new lib.camera(55, [7,-7,5], [64,0,47]);
 let viewport = new lib.viewport(process.stdout.columns, process.stdout.rows-1);
 
@@ -11,8 +11,8 @@ function viewportTest() {
 	let i=0;
 	while (i<viewport.height) {
 		if (i==0){
-			let a = lib.repeatStringNumTimes("0", (viewport.width-(fn.toString().length + 2)));
-			console.log(`${a}f=${fn}`);
+			let a = lib.repeatStringNumTimes("0", (viewport.width-(f.toString().length + 2)));
+			console.log(`${a}f=${f}`);
 		}else {
 			console.log("0");
 		}
