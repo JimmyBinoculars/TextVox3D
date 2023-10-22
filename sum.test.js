6534ec227862e06f6f36cd64e16bd22288dbd945
JimmyBinoculars <jimmy582263@hotmail.com>
2023-10-22 22:17:21 +0100

    Update test

---
+++ sum.test.js
@@ -0,0 +1,20 @@
+const lib = require("./index");
+
+test("Camera", () => {
+  expect(new lib.camera(55, 10, [7,-7,5], [64,0,47])).toEqual({"fl":55, "viewportHeight":10, "pos": [7,-7,5], "rot":[64,0,47]});
+});
+test("Mesh", () => {
+    expect(new lib.mesh([1,1,1], [1,1,1], [1,1,1])).toEqual({"points":[1,1,1], "edges":[1,1,1], "faces":[1,1,1]})
+});
+test("Item", () => {
+    expect(new lib.item("cube", [1,1,1], [0,0,0])).toEqual({"mesh":"cube", "pos":[1,1,1], "rot":[0,0,0]})
+});
+test("Viewport", () => {
+    expect(new lib.viewport(100, 100)).toEqual({"width":100, "height":100})
+});
+test("Repeat string func", () => {
+    expect(lib.repeatStringNumTimes("a", 3)).toEqual("aaa")
+});
+test("Get Mesh", () => {
+    expect(lib.getMesh("cube")).toEqual({"points":[[1,1,1], [-1,1,1], [-1,-1,1], [1,-1,1], [1,-1,-1], [1,1,-1], [-1,-1,-1], [-1,1,-1]], "edges":[[0,1], [0,5], [0,3], [1,2], [1,7], [2,3], [2,6], [3,4], [4,5], [4,6], [5,7], [7,6]]})
+});
