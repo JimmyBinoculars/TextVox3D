7bf03c865e8416ff41948433f2aabdc3bb8fc4ca
JimmyBinoculars <91781252+JimmyBinoculars@users.noreply.github.com>
2023-10-23 18:48:31 +0100

    eslint

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 exports.repeatStringNumTimes = function repeatStringNumTimes(string, times) {
-    var repeatedString = "";
-    while (times > 0) {
-      repeatedString += string;
-      times--;
-    }
-    return repeatedString;
+  let repeatedString = '';
+  while (times > 0) {
+    repeatedString += string;
+    times--;
+  }
+  return repeatedString;
 };
 exports.mesh = class mesh {
-    constructor(points, edges, faces) {
-        this.points = points;
-        this.edges = edges;
-        this.faces = faces;
-    }
-}
+  constructor(points, edges, faces) {
+    this.points = points;
+    this.edges = edges;
+    this.faces = faces;
+  }
+};
 exports.item = class item {
-	constructor(mesh, pos, rot) {
-		this.mesh = mesh;
-        this.pos = pos;
-        this.rot = rot;
-	}
+  constructor(mesh, pos, rot) {
+    this.mesh = mesh;
+    this.pos = pos;
+    this.rot = rot;
+  }
 };
 exports.camera = class camera {
-	constructor(fov, samples, pos, rot) {
-		this.fov = fov;
-        this.samples = samples;
-        this.pos = pos;
-        this.rot = rot;
-	}
+  constructor(fov, samples, pos, rot) {
+    this.fov = fov;
+    this.samples = samples;
+    this.pos = pos;
+    this.rot = rot;
+  }
 };
 exports.viewport = class viewport {
-	constructor(width, height) {
-		this.width = width;
-        this.height = height;
-	}
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
 };
 exports.getMesh = function getMesh(meshName) {
-    switch(meshName) {
-        case "cube" :
-            return {"points":[[1,1,1], [-1,1,1], [-1,-1,1], [1,-1,1], [1,-1,-1], [1,1,-1], [-1,-1,-1], [-1,1,-1]], "edges":[[0,1], [0,5], [0,3], [1,2], [1,7], [2,3], [2,6], [3,4], [4,5], [4,6], [5,7], [7,6]], "faces":[[1,3,5,2], [2,8,3,7], [7,5,9,6], [0,4,1,10], [3,4,6,11]]};
-    }
-}
+  switch (meshName) {
+    case 'cube':
+      return {'points': [[1, 1, 1], [-1, 1, 1], [-1, -1, 1], [1, -1, 1], [1, -1, -1], [1, 1, -1], [-1, -1, -1], [-1, 1, -1]], 'edges': [[0, 1], [0, 5], [0, 3], [1, 2], [1, 7], [2, 3], [2, 6], [3, 4], [4, 5], [4, 6], [5, 7], [7, 6]], 'faces': [[1, 3, 5, 2], [2, 8, 3, 7], [7, 5, 9, 6], [0, 4, 1, 10], [3, 4, 6, 11]]};
+  }
+};
 
 exports.raycast = function raycast(camera, viewport, items) {
-    return;
-}
+  return;
+};
