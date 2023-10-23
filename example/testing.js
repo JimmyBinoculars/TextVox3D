a5ffc3cb5baa9375d95092ff3aa5151e8e39f716
JimmyBinoculars <jimmy582263@hotmail.com>
2023-10-23 17:36:41 +0100

    Add comments to make issue easier to solve

--- example/testing.js
+++ example/testing.js
@@ -1,3 +1,4 @@
+/*Testing that I did whilst starting to make this (basically useless)
 let degrees = 70;
 console.log(`deg: ${degrees}`);
 let radians = degrees*(Math.PI/180);
@@ -5,28 +6,10 @@ console.log(`rad: ${radians}`);
 let tangent = Math.tan(radians);
 console.log(`tan: ${tangent}`);
 console.log(`calc: y=${tangent}x`);
-/*
-function isPointInPolygon(point, polygon) {
-    const x = point[0];
-    const y = point[1];
-    let isInside = false;
-
-    for (let i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
-        const xi = polygon[i][0];
-        const yi = polygon[i][1];
-        const xj = polygon[j][0];
-        const yj = polygon[j][1];
-
-        const intersect = yi > y !== yj > y && x < ((xj - xi) * (y - yi)) / (yj - yi) + xi;
-        if (intersect) {
-            isInside = !isInside;
-        }
-    }
-
-    return isInside;
-};*/
+*/
 
 function isPointInPolygon(x, y, polygon) {
+    //I know this only works for the square but if you are working on issue ticket #2 do not try to fix this unless it is what is causing the issue
     if (x >= polygon[0][0] && x <= polygon[1][0] && y >= polygon[0][1] && y <= polygon[2][1]) {
         return true;
     }else {
@@ -34,31 +17,49 @@ function isPointInPolygon(x, y, polygon) {
     }
 }
 
+//The field of view of the viewport (in degrees)
+let fov = 70;
+//Square is formatted like this [[point], [point]]
 let square = [[-0.25,4], [0.25, 4], [-0.25,6], [0.25,6]];
 let y = 0;
+//Max distance is set to low so it does not fill up the command prompt
 let maxDistance = 10;
+//Gets the dimentions of the viewport
 let viewport = {"height":process.stdout.rows-1, "width":process.stdout.columns};
 console.log(viewport);
 let castAngle = 0;
+//The current column that we are on
 let column = 0;
+//Columns where we have hit an object
 let columnHits = [];
 while(castAngle<=degrees) {
-    castAngle = ((viewport.width/(140))*column)-70;
+    //Calculate the exact angle that we need to cast at
+    castAngle = ((viewport.width/(fov*2))*column)-70;
+    //Convert the angle to a gradient for y=mx+c
     let castTang = Math.tan(castAngle*(Math.PI/180));
-    //console.log(`Cast angle: ${castAngle}, tang: ${castTang}`);
     while(y<maxDistance){
+        //Move the cursor for the raycast away from the camera
         y += 0.01;
+        //Calculate x using y=mx+c(ish), Maybe I've got this wrong?
         let x = castTang*y;
+        //Test if the current "raycast cursor" is inside/on the edge of an object
         if(isPointInPolygon(x, y, square)){
+            //Exit the loop (the 1000 is for assurance)
             y=maxDistance+1000;
+            //Add the current column to the list of columns that have hit an object
             columnHits += column;
         }
     };
+    //Reset y
     y=0;
+    //Move to the next column
     column++;
 };
+
+//Initialise the output variable
 let output = "";
 for(i=0;i<=(viewport.width-1);i++){
+    //Check if the current column is listed in the columns that have hit an object
     if(columnHits.includes(i)){
         output += "#";
     }else {
