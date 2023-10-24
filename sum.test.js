80c9c0555e7e9cd82afd4b626f0006698617aab0
JimmyBinoculars <91781252+JimmyBinoculars@users.noreply.github.com>
2023-10-24 08:58:15 +0100

    Update sum.test.js

--- sum.test.js
+++ sum.test.js
@@ -1,16 +1,16 @@
 const lib = require('./index');
 
 test('Camera', () => {
-  expect(new lib.camera(70, 10, [7, -7, 5], [64, 0, 47])).toEqual({'fov': 70, 'samples': 10, 'pos': [7, -7, 5], 'rot': [64, 0, 47]});
+  expect(new lib.Camera(70, 10, [7, -7, 5], [64, 0, 47])).toEqual({'fov': 70, 'samples': 10, 'pos': [7, -7, 5], 'rot': [64, 0, 47]});
 });
 test('Mesh', () => {
-  expect(new lib.mesh([1, 1, 1], [1, 1, 1], [1, 1, 1])).toEqual({'points': [1, 1, 1], 'edges': [1, 1, 1], 'faces': [1, 1, 1]});
+  expect(new lib.Mesh([1, 1, 1], [1, 1, 1], [1, 1, 1])).toEqual({'points': [1, 1, 1], 'edges': [1, 1, 1], 'faces': [1, 1, 1]});
 });
 test('Item', () => {
-  expect(new lib.item('cube', [1, 1, 1], [0, 0, 0])).toEqual({'mesh': 'cube', 'pos': [1, 1, 1], 'rot': [0, 0, 0]});
+  expect(new lib.Item('cube', [1, 1, 1], [0, 0, 0])).toEqual({'mesh': 'cube', 'pos': [1, 1, 1], 'rot': [0, 0, 0]});
 });
 test('Viewport', () => {
-  expect(new lib.viewport(100, 100)).toEqual({'width': 100, 'height': 100});
+  expect(new lib.Viewport(100, 100)).toEqual({'width': 100, 'height': 100});
 });
 test('Repeat string func', () => {
   expect(lib.repeatStringNumTimes('a', 3)).toEqual('aaa');
